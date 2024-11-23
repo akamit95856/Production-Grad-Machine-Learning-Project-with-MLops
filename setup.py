514ccda44a0602078670091dec5fe44680b6d4a7
akamit95856 <121858421+akamit95856@users.noreply.github.com>
2024-11-23 18:01:58 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Amit",
+    author_email="akamitnew958@gmail.com",
+    packages=find_packages()
+)
